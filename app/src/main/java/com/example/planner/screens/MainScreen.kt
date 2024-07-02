f693ac30e2774534fcd00c86279b3f491a17ad15
Philip Chen <philip.chen.0223@gmail.com>
2024-07-02 01:49:39 -0400

    Added navigation logic to app

--- app/src/main/java/com/example/planner/screens/MainScreen.kt
+++ app/src/main/java/com/example/planner/screens/MainScreen.kt
@@ -1,15 +1,24 @@
 package com.example.planner.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.planner.screens.agenda.AgendaScreen
 import com.example.planner.screens.calendar.CalendarScreen
 import com.example.planner.screens.schedule.ScheduleScreen
@@ -19,13 +28,25 @@ sealed class Screen(
     val route: String,
     val icon: ImageVector,
 ) {
-    object Agenda : Screen(name = "Agenda", route = "agenda", icon = Icons.Filled.Search)
-    object Calendar : Screen(name = "Calendar", route = "calendar", icon = Icons.Rounded.AddCircle)
-    object Schedule : Screen(name = "Schedule", route = "schedule", icon = Icons.Rounded.Person)
+    data object Agenda : Screen(name = "Agenda", route = "agenda", icon = Icons.Filled.Search)
+    data object Calendar : Screen(name = "Calendar", route = "calendar", icon = Icons.Rounded.AddCircle)
+    data object Schedule : Screen(name = "Schedule", route = "schedule", icon = Icons.Rounded.Person)
 }
 
 @Composable
-fun NavigationGraph(navController: NavHostController) {
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavBar(navController = navController) }
+    ) {
+        Box(modifier = Modifier.padding(it)) {
+            NavigationGraph(navController = navController)
+        }
+    }
+}
+
+@Composable
+private fun NavigationGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
         startDestination = Screen.Agenda.route
@@ -34,4 +55,32 @@ fun NavigationGraph(navController: NavHostController) {
         composable(Screen.Calendar.route) { CalendarScreen() }
         composable(Screen.Schedule.route) { ScheduleScreen() }
     }
+}
+
+@Composable
+private fun BottomNavBar(navController: NavHostController) {
+    val navBarItems = listOf(Screen.Agenda, Screen.Calendar, Screen.Schedule)
+    NavigationBar {
+        val currentRoute = getCurrentRoute(navController = navController)
+        navBarItems.forEach{
+            val isSelected =  it.route == currentRoute
+            NavigationBarItem(
+                icon = {
+                    Icon(imageVector = it.icon, contentDescription = "temp")
+                },
+                onClick = {
+                    if (!isSelected)
+                        navController.navigate(it.route)
+                },
+                selected = isSelected
+            )
+        }
+    }
+}
+
+// Helper Functions
+@Composable
+private fun getCurrentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
 }

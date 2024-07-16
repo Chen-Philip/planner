0d67fcde6f5e626b67d9286bd707b461ec80f69c
Philip Chen <philip.chen.0223@gmail.com>
2024-07-16 01:27:02 -0400

    Added modules and singletons for repository

--- app/src/main/java/com/example/planner/data/repository/user_repository/UserRepository.kt
+++ app/src/main/java/com/example/planner/data/repository/user_repository/UserRepository.kt
@@ -1,4 +1,4 @@
-package com.example.planner.data.repository
+package com.example.planner.data.repository.user_repository
 
 import com.example.planner.data.data_model.FirebaseTask
 import com.google.firebase.Firebase
@@ -6,24 +6,9 @@ import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.tasks.await
 
-class UserRepository {
-    val db = Firebase.firestore
-    val testUserId = "testUser"
+interface UserRepository {
+    suspend fun getTasks(): MutableList<FirebaseTask>
 
-    suspend fun getTasks(): MutableList<FirebaseTask> {
-        var test = mutableListOf<FirebaseTask>()
-        db.collection(testUserId).get().await().documents.forEach {
-            test.add(it.toObject<FirebaseTask>()!!)
-        }
-        return test
-    }
-    fun setTasks(tasks: List<FirebaseTask>) {
-        tasks.forEach {
-            if (it.id == null) {
-                val temp = db.collection(testUserId).document()
-                it.id = temp.id
-                db.collection(testUserId).document(it.id!!).set(it)
-            }
-        }
-    }
+    fun setTasks(tasks: List<FirebaseTask>)
 }
+
--- app/src/main/java/com/example/planner/di_module/FirebaseModule.kt
+++ app/src/main/java/com/example/planner/di_module/FirebaseModule.kt
@@ -12,5 +12,5 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
     @Provides
-    fun firestore (): FirebaseFirestore = Firebase.firestore
+    fun firestore() : FirebaseFirestore = Firebase.firestore
 }

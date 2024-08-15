342c20914fae13761182699b3ca369fd56529082
Philip Chen <philip.chen.0223@gmail.com>
2024-08-15 01:14:18 -0400

    Added test functions for retrieving tasks

--- app/src/main/java/com/example/planner/data/repository/user_repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/planner/data/repository/user_repository/UserRepositoryImpl.kt
@@ -1,8 +1,12 @@
 package com.example.planner.data.repository.user_repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.planner.data.User
 import com.example.planner.data.data_model.FirebaseTask
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
@@ -10,13 +14,20 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor (
     private val firestore: FirebaseFirestore
 ) : UserRepository {
-    override suspend fun getTasks(): MutableList<FirebaseTask> {
-        var test = mutableListOf<FirebaseTask>()
-        firestore.collection(User.userId).get().await().documents.forEach {
-            test.add(it.toObject<FirebaseTask>()!!)
+    override suspend fun getTasks(listener: (List<FirebaseTask>?, FirebaseFirestoreException?) -> Unit) {
+        firestore.collection(User.userId).addSnapshotListener { value, e ->
+            listener(value?.toObjects(FirebaseTask::class.java), e)
         }
-        return test
     }
+
+    override suspend fun getTasks2(listener: (List<FirebaseTask>?, FirebaseFirestoreException?) -> Unit): LiveData<List<FirebaseTask>> {
+        var tasks: MutableLiveData<List<FirebaseTask>> = MutableLiveData()
+        firestore.collection(User.userId).addSnapshotListener { value, e ->
+            tasks.postValue(value?.toObjects(FirebaseTask::class.java))
+        }
+        return tasks
+    }
+
     override fun setTasks(tasks: List<FirebaseTask>) {
         tasks.forEach {
             if (it.id == null) {

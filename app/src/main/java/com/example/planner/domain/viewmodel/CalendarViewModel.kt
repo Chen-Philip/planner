4748b28753b6727288667d7a7b324a11e107175a
Philip Chen <philip.chen.0223@gmail.com>
2024-09-07 16:40:52 -0400

    Made basic ui for calendar

--- app/src/main/java/com/example/planner/domain/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/example/planner/domain/viewmodel/CalendarViewModel.kt
@@ -1,16 +1,51 @@
 package com.example.planner.domain.viewmodel
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.planner.data.data_model.FirebaseTask
+import com.example.planner.data.dataclass.Task
 import com.example.planner.data.repository.user_repository.UserRepository
 import com.google.firebase.firestore.FieldValue
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Date
 import javax.inject.Inject
 
+
+@SuppressLint("SimpleDateFormat")
+@RequiresApi(Build.VERSION_CODES.O)
 @HiltViewModel
 class CalendarViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
+    val dateFormat: DateFormat = SimpleDateFormat("MMM, yyyy")
+    val dateTimeFormat = DateTimeFormatter.ofPattern("MMM, yyyy")
+
+    var tasks = mutableStateOf<List<Task>?>(listOf())
+
+    fun getTasks(date: LocalDate) {
+        viewModelScope.launch {
+            userRepository.getTasks { value, e ->
+                val temp = mutableListOf<Task>()
+                if (value != null) {
+                    for (task in value) {
+                        if (task.date.value != null && dateTimeFormat.format(date).equals(dateFormat.format(task.date.value!!))) {
+                            temp.add(task)
+                        }
+                    }
+                }
+                tasks.value = temp
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/planner/screens/planner/CalendarScreen.kt
+++ app/src/main/java/com/example/planner/screens/planner/CalendarScreen.kt
@@ -1,26 +1,83 @@
 package com.example.planner.screens.planner
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.planner.domain.viewmodel.AgendaViewModel
 import com.example.planner.domain.viewmodel.CalendarViewModel
+import com.example.planner.domain.viewmodel.MainScreenViewModel
+import com.example.planner.ui.custom_widgets.CustomSwitch
+import com.example.planner.ui.custom_widgets.TitleRow
+import java.util.Calendar
+import java.util.Date
+import kotlin.math.ceil
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun CalendarScreen(
-    calendarViewModel: CalendarViewModel = hiltViewModel()
+    calendarViewModel: CalendarViewModel = hiltViewModel(),
+    mainScreenViewModel: MainScreenViewModel,
 ) {
-
+    calendarViewModel.getTasks(mainScreenViewModel.date.value)
     Column {
-        Text(
-            text = "Calendar Screen"
+        TitleRow(
+            dateText = calendarViewModel.dateTimeFormat.format(mainScreenViewModel.date.value),
+            onPrevClick = { mainScreenViewModel.getPrevMonth() },
+            onNextClick = { mainScreenViewModel.getNextMonth() },
         )
-        Button(onClick = {  }) {
-            Text(
-                text = "Add Task"
-            )
+        var firstDayOfWeek = mainScreenViewModel.date.value.withDayOfMonth(1).dayOfWeek.value % 7
+        val lastDay = mainScreenViewModel.date.value.lengthOfMonth()
+        var numWeeks = ceil((lastDay + firstDayOfWeek) / 7.0).toInt() + 1
+        var currentDay = 1 - firstDayOfWeek
+
+        LazyColumn(modifier = Modifier.fillMaxSize(0.75f)) {
+            items(numWeeks) {
+                if (it == 0) {
+                    WeekdaysRow()
+                } else {
+                    Row {
+                        for (i in 0 .. 6) {
+                            if (currentDay < 1 || currentDay > lastDay) {
+                                Text(text = "_", modifier = Modifier.padding(5.dp))
+                            } else {
+                                Text(text = "$currentDay", modifier = Modifier.padding(5.dp))
+                            }
+                            currentDay++
+                        }
+                    }
+                }
+            }
         }
     }
-    
+
+}
+
+@Composable
+private fun WeekdaysRow() {
+    val weekdays = listOf(
+        "Sun",
+        "Mon",
+        "Tues",
+        "Wed",
+        "Thurs",
+        "Frr",
+        "Sat"
+    )
+
+    Row {
+       weekdays.forEach {
+           Text(it)
+       }
+    }
 }
